490cf42d2a54cc7cd1eeb0000305cd235caa9247
Tomás Pinho <me@tomaspinho.com>
2016-04-13 15:59:12 +0100

    adding support for doing automatic keymanagement inside docker container

--- routes/images.js
+++ routes/images.js
@@ -7,12 +7,17 @@ const crypto = require('crypto');
 const tar = require('tar-stream');
 const _ = require('underscore');
 
+const Docker = require('dockerode');
+const docker = new Docker();
+
 const rp = require('request-promise');
 
 module.exports = function (config, db, logger) {
 
   const updateServerOptions = {
     ca: fs.readFileSync(Path.join(__dirname, '..', config.update_server_ca_cert)),
+    cert: fs.readFileSync(Path.join(__dirname, '..', config.cert_path)),
+    key: fs.readFileSync(Path.join(__dirname, '..', config.key_path)),
     rejectUnauthorized: true,
   };
 
@@ -41,52 +46,81 @@ module.exports = function (config, db, logger) {
           logger.info('Server is signing a new update.');
 
           let fileOnDisk = fs.readFileSync(path_unsigned);
-          const sign = crypto.createSign('RSA-SHA512');
-          sign.write(fileOnDisk);
-          sign.end();
-          const privkey = fs.readFileSync(Path.join(__dirname, '..', config.image.signing_privkey), 'UTF-8');
-          const signedHash = sign.sign(privkey, 'base64');
 
-          logger.info('Server signed a new update, packing into tar.');
+          let signAndReply = () => {
+            const sign = crypto.createSign('RSA-SHA512');
+            sign.write(fileOnDisk);
+            sign.end();
+            const privkey = fs.readFileSync(Path.join(__dirname, '..', config.image.signing_privkey), 'UTF-8');
+            const signedHash = sign.sign(privkey, 'base64');
 
-          let signedFile = fs.createWriteStream(path_signed);
-          var pack = tar.pack();
-          pack.entry({ name: 'signature.txt' }, signedHash);
-          pack.entry({ name: 'image.img' }, fileOnDisk);
-          pack.finalize();
-          pack.pipe(signedFile);
+            logger.info('Server signed a new update, packing into tar.');
 
-          signedFile.on('finish', () => {
-            let options = _.extend({
-              url: `https://${config.update_server}/updates/`,
-              headers: {
-                Authorization: `Bearer ${config.update_server_ss_token}`,
-              },
-              formData: {
-                file: fs.createReadStream(path_signed),
-              },
-            }, updateServerOptions);
+            let signedFile = fs.createWriteStream(path_signed);
+            var pack = tar.pack();
+            pack.entry({ name: 'signature.txt' }, signedHash);
+            pack.entry({ name: 'image.img' }, fileOnDisk);
+            pack.finalize();
+            pack.pipe(signedFile);
 
-            logger.info('Uploading tar to update server.');
+            signedFile.on('finish', () => {
+              let options = _.extend({
+                url: `https://${config.update_server}/updates/`,
+                headers: {
+                  Authorization: `Bearer ${config.update_server_ss_token}`,
+                },
+                formData: {
+                  file: fs.createReadStream(path_signed),
+                },
+              }, updateServerOptions);
 
-            rp.post(options)
-              .then(function (response) {
-                logger.info(`Succesfully uploded image: ${path_signed}`);
-                return reply({ success: true });
-              })
-              .catch(function (err) {
-                logger.error('Error happened while uploading image \'%s\'',
-                  path_signed,
+              logger.info('Uploading tar to update server.');
+
+              rp.post(options)
+                .then(function (response) {
+                  logger.info(`Succesfully uploded image: ${path_signed}`);
+                  return reply({ success: true });
+                })
+                .catch(function (err) {
+                  logger.error('Error happened while uploading image \'%s\'',
+                    path_signed,
+                    err);
+                    return reply({ success: false });
+                  });
+            });
+
+            signedFile.on('error', (err) => {
+              logger.error('Something wrong happened while writing the signed ' +
+                'image file to disk', err);
+              return reply(Boom.badImplementation('Something wrong happened ' +
+                'while writing the signed image file to disk'));
+            });
+          }
+
+          /* Spawn docker container to take care of key management */
+          /* docker run --privileged=true -t -i -v $PWD/files:/opt/files/:ro
+                -v $IMAGE_PATH:/opt/image.img keymgmt
+          */
+          docker.run(config.image.container, [], process.stdout,
+            {
+              'HostConfig': {
+                'Privileged': true
+              }
+            },
+            {
+              'Binds': [
+                `${config.image.container_files}:/opt/files/:ro`,
+                `${path_unsigned}:/opt/image.img`
+              ],
+              'Privileged': true
+            }, function (err, data, container) {
+              if (err) {
+                logger.error('Got an error while running the keymgmt container',
                   err);
-                  return reply({ success: false });
-                });
-          });
-
-          signedFile.on('error', (err) => {
-            logger.error('Something wrong happened while writing the signed ' +
-              'image file to disk', err);
-            return reply(Boom.badImplementation('Something wrong happened ' +
-              'while writing the signed image file to disk'));
+                return;
+              }
+              logger.debug(data);
+              signAndReply();
           });
 
         });
